2a61a6bf073b3de379ab1a695c399a704795debd
feather2308 <feather2308@naver.com>
2025-10-10 23:28:58 +0900

    Flutter / SpringBoot / OCR / YOLO 구분함 ㅎㅎ

---
+++ SpringBoot/src/main/java/aise/legend_anabada/LegendAnabadaApplication.java
@@ -0,0 +1,13 @@
+package aise.legend_anabada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendAnabadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegendAnabadaApplication.class, args);
+    }
+
+}
---
+++ SpringBoot/src/test/java/aise/legend_anabada/LegendAnabadaApplicationTests.java
@@ -0,0 +1,13 @@
+package aise.legend_anabada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendAnabadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
